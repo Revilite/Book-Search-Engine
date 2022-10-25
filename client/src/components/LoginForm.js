4cd17346b2465d21c8ac2df94d3b095e56b786ee
Jacob Lockhart <procoder165@gmail.com>
2022-10-24 23:17:59 -0400

    working on fe again

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,18 +1,15 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/client"
 
-import Auth from '../utils/auth';
-
+import { LOGIN_USER } from "../utils/mutations"
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [login] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -30,11 +27,11 @@ const LoginForm = () => {
     }
 
     try {
-      const { data } = await login({
-        variables: { ...userFormData },
-      });
-
-      Auth.login(data.login.token);
+      const { data } = login(
+        {variables: {...userFormData}}
+      )
+      
+      console.log(data);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
