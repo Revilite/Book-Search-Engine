569529e5a43fe0078ec5d64237423942d9257e17
Jacob Lockhart <procoder165@gmail.com>
2022-10-26 23:34:15 -0400

    fixed commit

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -31,11 +31,11 @@ const SignupForm = () => {
     }
 
     try {
-      const data = await addUser({
+      const { data } = await addUser({
         variables: { ...userFormData }
       });
       console.log(data.addUser);
-      Auth.login(data.addUser);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
